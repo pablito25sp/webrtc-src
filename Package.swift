ba5cf6196a1067b5aeb21b6ecb8b503b76d2e6d8
Pablo San Pietro <psanpietro@gmail.com>
2021-06-11 09:42:47 -0300

    Swift package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "WebRTC",
+  products: [
+    .library(
+      name: "WebRTC",
+      targets: ["WebRTC"]
+    )
+  ],
+  dependencies: [],
+  targets: [
+      .binaryTarget(
+          name: "WebRTC",
+          path: "WebRTC.framework"
+      )
+  ]
+)
